added options for normalising images in plotting and (hopefully) fix a bug the normalisation can produce

src/spacepylot/plotting.py
@@ -21,7 +21,7 @@ import colorcet as cc
 
 from . import alignment_utilities as au
 from . import fitsTools as fT
-from .utils import get_polynorm, my_linear_model, filtermed_image
+from .utils import get_polynorm_SP, my_linear_model, filtermed_image
 
 
 vector_plot_properties = {
@@ -210,14 +210,14 @@ def crop_data(data, border=10):
     """Crop a 2D data and return it cropped after a border
     has been removed (number of pixels) from each edge
     (borderx2 pixels are removed from each dimension)
-    
+
     Input
     -----
     data: 2d array
         Array which has the signal to be cropped
     border: int
         Number of pixels to be cropped at each edge
-    
+
     Returns
     -------
     cdata: 2d array
@@ -242,14 +242,14 @@ def crop_data(data, border=10):
 def get_flux_range(data, low=2, high=98, border=25):
     """Get the range of fluxes within the array
     by looking at percentiles.
-     
+
     Input
     -----
     data: 2d array
         Input array with signal to process
     low, high: two floats (10, 99)
         Percentiles to consider to filter
-    
+
     Returns
     -------
     lperc, hperc: 2 floats
@@ -269,7 +269,7 @@ def get_flux_range(data, low=2, high=98, border=25):
     return lperc, hperc
 
 
-def get_ax_contours(comp_image, ref_image, ax=None, title='', 
+def get_ax_contours(comp_image, ref_image, ax=None, title='',
                   **kwargs):
     """Show the contours for two images
 
@@ -300,11 +300,11 @@ def get_ax_contours(comp_image, ref_image, ax=None, title='',
         # Find the high and low levels
         lowlevel, highlevel = get_flux_range(comp_image)
         levels = np.linspace(np.log10(lowlevel),
-                             np.log10(highlevel), 
+                             np.log10(highlevel),
                              nlevels)
 
     # plotting the first contour - comparison image
-    compset = ax.contour(np.log10(comp_image), levels, colors='k', 
+    compset = ax.contour(np.log10(comp_image), levels, colors='k',
                          origin='lower', linestyles='solid')
     # plotting the second contour - reference image
     refset = ax.contour(np.log10(ref_image), levels=levels,
@@ -434,6 +434,20 @@ def plot_image(image, ax=None, title='', vmin_perc=5, vmax_perc=95,
     else:
         return ax
 
+default_polynorm_SP_params = {
+    'chunk_size':15,
+    'threshold1':0.,
+    'threshold2':0,
+    'percentiles':(0., 100.),
+    'sigclip':0,
+    'perc_if_fail':25
+}
+
+def _pop_norm_param_for_get_polynorm_SP(kwargs):
+    polynorm_SP_kwargs = {}
+    for key in default_polynorm_SP_params.keys():
+        polynorm_SP_kwargs[key] = kwargs.pop(key,default_polynorm_SP_params[key])
+    return polynorm_SP_kwargs
 
 prealign_titles = {
     'red-blue': 'Prealign-red, reference-cyan',
@@ -656,7 +670,7 @@ class AlignmentPlotting:
         return cls(data_prealign, data_reference, rotation, shifts,
                    header, v, u, rotation_fit)
 
-    def red_blue_before_after(self, normalise=True, filtermed=True):
+    def red_blue_before_after(self, normalise=True, filtermed=True, **kwargs):
         """Plots the alignment before and after applying the offsets with
         red showing the prealign/align image and cyan showing the reference image
         Image is well aligned if the aligned image is grey scale with no visible
@@ -671,7 +685,8 @@ class AlignmentPlotting:
             Matplotlib axis object for the aligned image
         """
         if normalise:
-            poly = get_polynorm(self.aligned, self.reference)
+            poly_kwargs = _pop_norm_param_for_get_polynorm_SP(kwargs)
+            poly = get_polynorm_SP(self.aligned, self.reference, **poly_kwargs)
             prealign = my_linear_model(poly.beta, self.prealign)
             aligned = my_linear_model(poly.beta, self.aligned)
         else:
@@ -683,7 +698,7 @@ class AlignmentPlotting:
             prealign = filtermed_image(prealign)
             aligned = filtermed_image(aligned)
             reference = filtermed_image(reference)
-        
+
 
         rgb_before = make_red_blue_overlay(prealign, reference)
         rgb_after = make_red_blue_overlay(aligned, reference)
@@ -705,7 +720,7 @@ class AlignmentPlotting:
 
         return ax0, ax1
 
-    def plot_divcontours(self, nlevels=10, levels=None, percentage=5.0, filtermed=True, normalise=True):
+    def plot_divcontours(self, nlevels=10, levels=None, percentage=5.0, filtermed=True, normalise=True, **kwargs):
         """Plots the alignment after the offsets by dividing the input reference
         image and the aligned one, and showing the contours.
         Offsets should show up as negative/positive residuals systematically on
@@ -730,7 +745,8 @@ class AlignmentPlotting:
         """
 
         if normalise:
-            poly = get_polynorm(self.aligned, self.reference)
+            poly_kwargs = _pop_norm_param_for_get_polynorm_SP(kwargs)
+            poly = get_polynorm_SP(self.aligned, self.reference, **poly_kwargs)
             aligned = my_linear_model(poly.beta, self.aligned)
         else:
             aligned = self.aligned * 1.0
@@ -741,16 +757,16 @@ class AlignmentPlotting:
             reference = filtermed_image(reference)
 
         after = reference - aligned
-        
+
         fig, axs = initialise_fig_ax(fig_name='divcontours',
                                      fig_size=self.fig_size,
                                      header=self.header,
                                      grid=[1, 2])
 
-        ax0 = get_ax_division(aligned, reference, ax=axs[0], 
+        ax0 = get_ax_division(aligned, reference, ax=axs[0],
                               title=titles['after']['division'],
                               percentage=percentage)
-        ax1 = get_ax_contours(aligned, reference, ax=axs[1], 
+        ax1 = get_ax_contours(aligned, reference, ax=axs[1],
                               title=titles['after']['contours'],
                               nlevels=nlevels, levels=levels)
 
@@ -791,7 +807,8 @@ class AlignmentPlotting:
         """
 
         if normalise:
-            poly = get_polynorm(self.aligned, self.reference)
+            poly_kwargs = _pop_norm_param_for_get_polynorm_SP(kwargs)
+            poly = get_polynorm_SP(self.aligned, self.reference, **poly_kwargs)
             prealign = my_linear_model(poly.beta, self.prealign)
             aligned = my_linear_model(poly.beta, self.aligned)
         else:
@@ -861,7 +878,8 @@ class AlignmentPlotting:
         """
 
         if normalise:
-            poly = get_polynorm(self.aligned, self.reference)
+            poly_kwargs = _pop_norm_param_for_get_polynorm_SP(kwargs)
+            poly = get_polynorm_SP(self.aligned, self.reference, **poly_kwargs)
             prealign = my_linear_model(poly.beta, self.prealign)
             aligned = my_linear_model(poly.beta, self.aligned)
         else:

src/spacepylot/utils.py
@@ -195,7 +195,7 @@ def _split(size, num):
         Upper index position of a given length.
 
     """
-    lower, upper = int(size) * np.array([num, num+1])
+    lower, upper = np.int(size) * np.array([num, num+1])
     return lower, upper
 
 
@@ -210,7 +210,7 @@ def reject_outliers(data, m=3):
         Input factor which gives the number of standard deviations
         beyond which points will be rejected.
     """
-    return data[abs(data - np.median(data)) < m * np.std(data)]
+    return data[np.abs(data - np.nanmedian(data)) < m * np.nanstd(data)]
 
 
 def _remove_nonvalid_numbers(image):
@@ -276,30 +276,31 @@ def chunk_stats(list_arrays, chunk_size=15):
         for the given datasets analysed in chunks.
 
     """
+    # Check that all arrays have the same size
+    if not np.all([list_arrays[0].size == d.size for d in list_arrays[1:]]):
+        #Relic from pymusepipe. Changing to raise error
+        # upipe.print_error("Datasets are not of the same "
+        #                   "size in median_compare")
+        raise IndexError("Arrays given in `list_arrays` are not of the same"\
+                          "size in `chunk_stats`")
 
+    list_arrays = np.atleast_3d(list_arrays)
     narrays = len(list_arrays)
 
-    nchunk_x = np.int(list_arrays[0].shape[0] // chunk_size - 1)
-    nchunk_y = np.int(list_arrays[0].shape[1] // chunk_size - 1)
-    # Check that all arrays have the same size
-    med_array = np.zeros((narrays, nchunk_x * nchunk_y), dtype=np.float64)
-    std_array = np.zeros_like(med_array)
+    nchunk_x = np.int(list_arrays[0].shape[0] // chunk_size) #-1)
+    nchunk_y = np.int(list_arrays[0].shape[1] // chunk_size) #-1)
 
-    if not all([d.size for d in list_arrays]):
-        upipe.print_error("Datasets are not of the same "
-                          "size in median_compare")
-    else:
-        for i in range(0, nchunk_x):
-            for j in range(0, nchunk_y):
-                for k in range(narrays):
-                    # Taking the median of all arrays
-                    med_array[k, i * nchunk_y + j] = np.nanmedian(
-                        list_arrays[k][i * chunk_size:(i + 1) * chunk_size,
-                        j * chunk_size:(j + 1) * chunk_size])
-                    # Taking the std deviation of all arrays
-                    std_array[k, i * nchunk_y + j] = mad_std(
-                        list_arrays[k][i * chunk_size:(i + 1) * chunk_size,
-                        j * chunk_size:(j + 1) * chunk_size], ignore_nan=True)
+    grid_number = nchunk_x * nchunk_y
+
+    #Vectorised to remove 3-nest loop
+    arrays3d_chunk_ready = list_arrays[:,:nchunk_x*chunk_size,:nchunk_y*chunk_size]
+
+    grids_nchunkx  = np.array(np.split(arrays3d_chunk_ready, nchunk_x, axis=1))
+
+    grids_xy = np.array(np.split(np.array(grids_nchunkx), nchunk_y, axis=-1))
+
+    med_array = np.nanmedian(grids_xy, axis=(-2,-1)).T.reshape(narrays, grid_number)
+    std_array = mad_std(grids_xy, axis=(-2,-1), ignore_nan=True).T.reshape(narrays, grid_number)
 
     # Cleaning in case of Nan
     med_array = np.nan_to_num(med_array)
@@ -307,8 +308,9 @@ def chunk_stats(list_arrays, chunk_size=15):
     return med_array, std_array
 
 
-def get_polynorm(array1, array2, chunk_size=15, threshold1=0.,
-                        threshold2=0, percentiles=(0., 100.), sigclip=0):
+def get_polynorm_SP(array1, array2, chunk_size=15, threshold1=0.,
+                        threshold2=0, percentiles=(0., 100.), sigclip=0,
+                        perc_if_fail=25):
     """Find the normalisation factor between two arrays.
 
     Including the background and slope. This uses the function
@@ -331,6 +333,9 @@ def get_polynorm(array1, array2, chunk_size=15, threshold1=0.,
         Percentiles (Default value = [0., 100.])
     sigclip : float
         Sigma clipping factor (Default value = 0)
+    perc_if_fail : float, optional
+        If the (default) threshold of 0 is too high, a threshold
+        based of the percentiles (1-100) is used. The default is 25.
 
     Returns
     -------
@@ -342,16 +347,13 @@ def get_polynorm(array1, array2, chunk_size=15, threshold1=0.,
     med, std = chunk_stats([array1, array2], chunk_size=chunk_size)
 
     # Selecting where data is supposed to be good
-    if threshold1 is None:
-        threshold1 = 0.
-    if threshold2 is None:
-        threshold2 = 0.
-    pos = (med[0] > threshold1) & (std[0] > 0.) & (std[1] > 0.) & (med[1] > threshold2)
+    pos = _get_good_pos_for_polynorm([threshold1,threshold2], med, std, perc_if_fail)
 
     # Guess the slope from this selection
     guess_slope = 1.0
 
     # Doing the regression itself
+
     result = regress_odr(x=med[0][pos], y=med[1][pos], sx=std[0][pos],
                          sy=std[1][pos], beta0=[0., guess_slope],
                          percentiles=percentiles, sigclip=sigclip)
@@ -359,6 +361,45 @@ def get_polynorm(array1, array2, chunk_size=15, threshold1=0.,
     result.std = std
     result.selection = pos
     return result
+
+def _get_good_pos_for_polynorm(thresholds, med, std, perc_if_fail=25):
+    """Applies the threshold for median-like filtered chunks. If None, first
+    assume all good values are > 0. If no chunks are >0, a percentage
+    is chosen as the threshold value. The default is 25 for this
+    percentage.
+
+    Parameters
+    ----------
+    thresholds : arraylike
+        Thresholds to apply to each chunk to determine if it is good.
+    med : numpy.ndarray
+        Median values from a median-like filtered image separated
+        into chunks.
+    std : numpy.ndarray
+        standard deviation values from a median-like filtered image separated
+        into chunks.
+    perc_if_fail : float, optional
+        If the (default) threshold of 0 is too high, a threshold
+        based of the percentiles (1-100) is used. The default is 25.
+
+    Returns
+    -------
+    pos_good : numpy.ndarray of bool
+        Boolian mask containing True where chunks are good and above
+        the threshold value.
+
+    """
+    pos_good = np.ones(len(med[0]), dtype=bool)
+    for i in range(len(med)):
+        current_threshold = thresholds[i] if thresholds[i] is not None else 0
+        current_pos = (med[i] >= current_threshold) & (std[i] >= 0.)
+        if len(current_pos) == 0:
+            current_threshold = np.nanpercentile(med[i], perc_if_fail)
+            current_pos = (med[i] > current_threshold) & (std[i] >= 0.)
+
+        pos_good = pos_good & current_pos
+
+    return pos_good
 
 
 def my_linear_model(B, x):
@@ -406,7 +447,7 @@ def regress_odr(x, y, sx, sy, beta0=(0., 1.),
     # Percentiles
     xrav = x.ravel()
     if len(xrav) > 0:
-        percentiles = np.percentile(xrav, percentiles)
+        percentiles = np.nanpercentile(xrav, percentiles)
         sel = (xrav >= percentiles[0]) & (xrav <= percentiles[1])
     else:
         sel = np.abs(xrav) > 0